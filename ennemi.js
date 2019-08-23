7f0f134de4cd3d7fab33fc0b2d378ff2adf6332c
Saida Zghal <zghal.saida@orange.fr>
2019-08-23 21:21:17 +0200

    fonction templateEnnemi

--- ennemi.js
+++ ennemi.js
@@ -1,32 +1,48 @@
-var ennemi1 = {
-  nom: "Turtle",
-  description: "Tortue du sud",
-  level: 1,
-  force: 2,
+var templateEnnemi = {
+  creerTurtle: function() {
+    var ennemi1 = {
+      nom: "Turtle",
+      description: "Tortue du sud",
+      level: 1,
+      force: 2,
 
-  afficherEnnemi: function() {
-    var txt = "Nom : " + this.nom + "\n";
-    txt += "Description : " + this.description + "\n";
-    txt += "Level : " + this.level + "\n";
-    txt += "Force : " + this.force;
-    console.log(txt);
+      afficherEnnemi: function() {
+        var txt = "Nom : " + this.nom + "\n";
+        txt += "Description : " + this.description + "\n";
+        txt += "Level : " + this.level + "\n";
+        txt += "Force : " + this.force;
+        console.log(txt);
+      }
+    };
+    return ennemi1;
+  },
+
+  creerNinja: function() {
+    var ennemi2 = {
+      nom: "Ninja",
+      description: "Ninja du nord",
+      level: 2,
+      force: 4,
+
+      afficherEnnemi: function() {
+        var txt = "Nom : " + this.nom + "\n";
+        txt += "Description : " + this.description + "\n";
+        txt += "Level : " + this.level + "\n";
+        txt += "Force : " + this.force;
+        console.log(txt);
+      }
+    };
+    return ennemi2;
   }
 };
 
-var ennemi2 = {
-  nom: "Ninja",
-  description: "Ninja du nord",
-  level: 2,
-  force: 4,
+var turtle1 = templateEnnemi.creerTurtle();
+var turtle2 = templateEnnemi.creerTurtle();
+turtle2.level = 2;
+turtle2.force++;
 
-  afficherEnnemi: function() {
-    var txt = "Nom : " + this.nom + "\n";
-    txt += "Description : " + this.description + "\n";
-    txt += "Level : " + this.level + "\n";
-    txt += "Force : " + this.force;
-    console.log(txt);
-  }
-};
+turtle1.afficherEnnemi();
+turtle2.afficherEnnemi();
 
-ennemi1.afficherEnnemi();
-ennemi2.afficherEnnemi();
+var ninja1 = templateEnnemi.creerNinja();
+ninja1.afficherEnnemi();
