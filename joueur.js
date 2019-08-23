c203b49a48c874d449ee2c38295e39d8a1ac77a4
Saida Zghal <zghal.saida@orange.fr>
2019-08-23 17:46:19 +0200

    creation d'objet carac dans objet joueur

--- joueur.js
+++ joueur.js
@@ -1,15 +1,28 @@
 var joueur = {
   nom: "Rahan",
   description: "Le meilleur des guerriers",
-  force: 5,
-  agilite: 3,
-  intell: 2
+  caracteres: {
+    force: 5,
+    agilite: 3,
+    intell: 2
+  }
 };
+// console.log(joueur.nom);
+// console.log(joueur.description);
+// console.log(joueur.caracteres.force);
+// console.log(joueur.caracteres.agilite);
+// console.log(joueur.caracteres.intell);
 
 afficherInformationJoueur(joueur);
 
 function afficherInformationJoueur(j) {
   for (var propriete in j) {
-    console.log(propriete + " : " + j[propriete]);
+    if (typeof j[propriete] !== "object") {
+      console.log(propriete + " : " + j[propriete]);
+    } else {
+      for (var subPropriete in j[propriete]) {
+        console.log(subPropriete + " : " + j[propriete][subPropriete]);
+      }
+    }
   }
 }
